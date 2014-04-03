d26e34cd8cd76f7720b2812feb0f4646935a1862
Peter Forgacs <peter.forgacs@spilgames.com>
2014-04-03 13:36:43 +0200

    Mouse event development

--- js/mouse.js
+++ js/mouse.js
@@ -1,7 +1,5 @@
 var MOUSE_STATE_NULL = 0;
-var MOUSE_STATE_AXIS_X = 10;
-var MOUSE_STATE_AXIS_Y = 11;
-var MOUSE_STATE_AXIS_Z = 12;
+var MOUSE_STATE_AXIS = 10;
 var MOUSE_STATE_CLICK = 20;
 var MOUSE_STATE_CLICK_CAPTURED = 21;
 var MOUSE_STATE_CLICK_RELEASED = 22;
@@ -14,12 +12,11 @@ var mouseYOnMouseDown = 0;
 var mouseXDelta = 0;
 var mouseYDelta = 0;
 
-var piHalf = (Math.PI / 2);
-
 var projector = new THREE.Projector();
 var clickedObjects;
 
 document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+
 document.addEventListener( 'touchstart', onDocumentTouchStart, false );
 document.addEventListener( 'touchmove', onDocumentTouchMove, false );
 document.addEventListener( 'touchend', onDocumentTouchEnd, false );
@@ -27,13 +24,24 @@ document.addEventListener( 'touchend', onDocumentTouchEnd, false );
 function onDocumentMouseDown( event ) {
     event.preventDefault();
     
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    
+    clickWithMouse(mouseX, mouseY);
+    
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+    document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+}
+
+function clickWithMouse(mouseX, mouseY) {
     if (MOUSE_STATE_CLICK_RELEASED == mouseState) {
-    	mouseState = MOUSE_STATE_NULL;
+        mouseState = MOUSE_STATE_NULL;
     }
     
-    var intersects = getClickTargetObjects(event.clientX, event.clientY);
+    var intersects = getClickTargetObjects(mouseX, mouseY);
     
-    if ( intersects.length > 1 ) {
+    if ( intersects.length > 0 ) {
         var found = -1;
         for (var i = 0; i < cubePage.length; i++) {
             if (cubePage[i].id == intersects[ 0 ].object.id) {
@@ -46,18 +54,24 @@ function onDocumentMouseDown( event ) {
             clickedObjects = intersects;
         }
     }
+    else {
+        mouseState = MOUSE_STATE_AXIS;
+    }
     
-    mouseXOnMouseDown = event.clientX - windowHalfX;
-    mouseYOnMouseDown = event.clientY - windowHalfY;
-    
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+    mouseXOnMouseDown = mouseX - windowHalfX;
+    mouseYOnMouseDown = mouseY - windowHalfY;
 }
 
 function onDocumentMouseMove( event ) {
-    mouseX = event.clientX - windowHalfX;
-    mouseY = event.clientY - windowHalfY;
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    
+    moveWithMouse(mouseX, mouseY);
+}
+
+function moveWithMouse(mouseX, mouseY) {
+    mouseX = mouseX - windowHalfX;
+    mouseY = mouseY - windowHalfY;
     
     if (MOUSE_STATE_CLICK == mouseState) {
         var intersects = getClickTargetObjects(event.clientX, event.clientY);
@@ -69,8 +83,6 @@ function onDocumentMouseMove( event ) {
                 }
             }
             if (found >= 0) {
-                console.warn('x', clickedObjects[ 1 ].object.position, clickedObjects[ 0 ].object.position, intersects[ 0 ].object.position);
-                
                 mouseState = MOUSE_STATE_CLICK_CAPTURED;
                 
                 var point = clickedObjects[ 0 ].point;
@@ -89,50 +101,51 @@ function onDocumentMouseMove( event ) {
                 var x = 1 * (Math.floor(Math.abs(pointX) / 0.2) - 1);
                 var y = -1 * (Math.floor(Math.abs(pointY) / 0.2) - 1);
                 var z = -1 * (Math.floor(Math.abs(pointX) / 0.2) - 1);
+console.warn(pointX, pointY);
                 
                 rotatePage(x, y, z, x1 == x2, y1 == y2, z1 == z2, y1 > y2, z1 < z2, y1 > y2);
             }
         }
     }
-    else if (MOUSE_STATE_CLICK_CAPTURED == mouseState || MOUSE_STATE_CLICK_RELEASED == mouseState) {
-        // Do nothing.
-    }
-    else {
+    else if (MOUSE_STATE_AXIS == mouseState) {
         mouseXDelta = mouseX - mouseXOnMouseDown;
         mouseYDelta = mouseY - mouseYOnMouseDown;
         
-        if (MOUSE_STATE_NULL == mouseState && ((Math.abs(mouseXDelta) > 10 || Math.abs(mouseYDelta) > 10))) {
+        var axis = '';
+        if ((Math.abs(mouseXDelta) > 10 || Math.abs(mouseYDelta) > 10)) {
             if (Math.abs(mouseXDelta) > Math.abs(mouseYDelta)) {
-                mouseState = MOUSE_STATE_AXIS_Y;
+                axis = 'y';
             }
             else {
-                mouseState = (mouseX < 0 ? MOUSE_STATE_AXIS_Z : MOUSE_STATE_AXIS_X);
+                axis = (mouseX < 0 ? 'z' : 'x');
             }
-        }
-        
-        var x = y = z = 0;
-        var xStatic = yStatic = zStatic = false;
-        var xDirection = yDirection = zDirection = null;
-        if (MOUSE_STATE_AXIS_X == mouseState) {
-            xStatic = true;
-            xDirection = (mouseYDelta >= 0);
-            for (var x = -1; x <= 1; x++) {
-                rotatePage(x, y, z, xStatic, yStatic, zStatic, xDirection, yDirection, zDirection);
+            
+            var x = y = z = 0;
+            var xStatic = yStatic = zStatic = false;
+            var xDirection = yDirection = zDirection = null;
+            if ('x' == axis) {
+                xStatic = true;
+                xDirection = (mouseYDelta >= 0);
+                for (var x = -1; x <= 1; x++) {
+                    rotatePage(x, y, z, xStatic, yStatic, zStatic, xDirection, yDirection, zDirection);
+                }
             }
-        }
-        else if (MOUSE_STATE_AXIS_Y == mouseState) {
-            yStatic = true;
-            yDirection = (mouseXDelta >= 0);
-            for (var y = -1; y <= 1; y++) {
-                rotatePage(x, y, z, xStatic, yStatic, zStatic, xDirection, yDirection, zDirection);
+            else if ('y' == axis) {
+                yStatic = true;
+                yDirection = (mouseXDelta >= 0);
+                for (var y = -1; y <= 1; y++) {
+                    rotatePage(x, y, z, xStatic, yStatic, zStatic, xDirection, yDirection, zDirection);
+                }
             }
-        }
-        else if (MOUSE_STATE_AXIS_Z == mouseState) {
-            zStatic = true;
-            zDirection = (mouseYDelta >= 0);
-            for (var z = -1; z <= 1; z++) {
-                rotatePage(x, y, z, xStatic, yStatic, zStatic, xDirection, yDirection, zDirection);
+            else if ('z' == axis) {
+                zStatic = true;
+                zDirection = (mouseYDelta >= 0);
+                for (var z = -1; z <= 1; z++) {
+                    rotatePage(x, y, z, xStatic, yStatic, zStatic, xDirection, yDirection, zDirection);
+                }
             }
+            
+            mouseState = MOUSE_STATE_NULL;
         }
     }
 }
@@ -144,25 +157,19 @@ function onDocumentMouseUp( event ) {
 }
 
 function onDocumentMouseOut( event ) {
-    //document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-    //document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-    //document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
 }
 
 function onDocumentTouchStart( event ) {
     if ( event.touches.length === 1 ) {
         event.preventDefault();
         
-        var intersects = getClickTargetObjects(event.touches[ 0 ].pageX, event.touches[ 0 ].pageY);
+        mouseX = event.touches[ 0 ].pageX;
+        mouseY = event.touches[ 0 ].pageY;
         
-        if ( intersects.length > 1 ) {
-            mouseState = MOUSE_STATE_CLICK;
-            
-            //intersects[ 0 ].object.position = {x: 5, y: 0, z: 0};
-        }
-        
-        mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
-        mouseYOnMouseDown = event.touches[ 0 ].pageY - windowHalfY;
+        clickWithMouse(mouseX, mouseY);
     }
 }
 
@@ -170,32 +177,10 @@ function onDocumentTouchMove( event ) {
     if ( event.touches.length === 1 ) {
         event.preventDefault();
         
-        if (MOUSE_STATE_CLICK == mouseState) {
-            // TODO
-        }
-        else {
-            mouseX = event.touches[ 0 ].pageX - windowHalfX;
-            mouseY = event.touches[ 0 ].pageY - windowHalfY;
-            
-            mouseXDelta = mouseX - mouseXOnMouseDown;
-            mouseYDelta = mouseY - mouseYOnMouseDown;
-            
-            if (MOUSE_STATE_NULL == mouseState && ((Math.abs(mouseXDelta) > 10 || Math.abs(mouseYDelta) > 10))) {
-                if (Math.abs(mouseXDelta) > Math.abs(mouseYDelta)) {
-                    mouseState = MOUSE_STATE_AXIS_Y;
-                }
-                else {
-                    mouseState = (mouseX < 0 ? MOUSE_STATE_AXIS_Z : MOUSE_STATE_AXIS_X);
-                }
-            }
-            
-            if (MOUSE_STATE_AXIS_X == mouseState) {
-            }
-            else if (MOUSE_STATE_AXIS_Y == mouseState) {
-            }
-            else if (MOUSE_STATE_AXIS_Z == mouseState) {
-            }
-        }
+        mouseX = event.touches[ 0 ].pageX;
+        mouseY = event.touches[ 0 ].pageY;
+        
+        moveWithMouse(mouseX, mouseY);
     }
 }
 
