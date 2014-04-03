8b68ff217e61f05c008816375d661ed5218f8987
Peter Forgacs <peter.forgacs@spilgames.com>
2014-04-03 10:49:46 +0200

    Rotation development

--- js/mouse.js
+++ js/mouse.js
@@ -95,7 +95,7 @@ function onDocumentMouseMove( event ) {
                 var x2 = intersects[ 0 ].object.position.x;
                 var y2 = intersects[ 0 ].object.position.y;
                 var z2 = intersects[ 0 ].object.position.z;
-                rotatePage(mouseX, mouseY, x1, y1, z1, x2, y2, z2);
+                rotatePage(mouseX, mouseY, x1 == x2, y1 == y2, z1 == z2, y1 > y2, z1 < z2, y1 > y2);
             }
         }
     }
