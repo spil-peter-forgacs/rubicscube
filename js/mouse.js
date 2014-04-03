f2fbd722f2a3f1de4c15d9dfe6767027f4eac915
Peter Forgacs <peter.forgacs@spilgames.com>
2014-04-03 15:08:42 +0200

    Mouse development

--- js/mouse.js
+++ js/mouse.js
@@ -120,6 +120,9 @@ function moveWithMouse(mouseX, mouseY) {
                         
                         rotatePage(x, y, z, x1 == x2, y1 == y2, z1 == z2, y1 > y2, z1 < z2, y1 > y2);
                     }
+                    else {
+                        mouseState = MOUSE_STATE_CLICK_RELEASED;
+                    }
                 }
             }
         }
